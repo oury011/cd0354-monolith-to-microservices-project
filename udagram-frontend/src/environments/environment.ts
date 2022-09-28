d8fb74fb00965133f6547850a0f5a1eb859b116a
Diallo <dialloamadou10@yahoo.com>
2022-09-28 02:51:03 +0000

    adding build and push pipeline

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa8231f6cdf36428a8aaabdee172ea96-1534363395.us-east-1.elb.amazonaws.com'
 };
 
 /*
